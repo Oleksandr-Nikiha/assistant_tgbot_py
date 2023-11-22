532c70246829632102b8befe20e913ead3c70246
nikiga <oleksandr.nikiga@fuib.com>
2023-11-22 18:59:08 +0200

    Minor fixes

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 
-import config
+from . import config
 
 dp = Dispatcher(BaseStorage=MemoryStorage)
 bot = Bot(token=config.BOT_TOKEN, parse_mode=ParseMode.HTML)
