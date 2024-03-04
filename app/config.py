a629e1fe185eb38516415b83b4ed31cef54c1de1
LuckerDie <fenixdarksreal@gmail.com>
2024-03-04 22:21:18 +0200

    Change config.py

--- app/config.py
+++ app/config.py
@@ -4,13 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN: str = os.getenv('BOT_TOKEN')
-NBU_URL: str = os.getenv('NBU_URL')
-PB_URL: str = os.getenv('PB_CURRENCY')
-
-WEATHER_URL: str = os.getenv('WEATHER_URL')
-CONDITION_URL: str = os.getenv('CONDITION_URL')
 WEATHER_TOKEN: str = os.getenv('WEATHER_TOKEN')
-KEY_PATH: str = os.getenv('KEY_PATH')
+
+PB_URL: str = 'https://api.privatbank.ua/p24api/exchange_rates?json&date='
+WEATHER_URL: str = 'https://api.weatherapi.com/v1/current.json?q='
+CONDITION_URL: str = 'https://www.weatherapi.com/docs/conditions.json'
+
 MONGO_USER: str = os.getenv('MONGO_USER')
 MONGO_PASSWORD: str = os.getenv('MONGO_PASSWORD')
 MONGO_URL: str = os.getenv('MONGO_URL')
